f8212dc1df3fe883a79b4b1b32a7e7b3d13b6fe5
arunkumar6702 <113006398+arunkumar6702@users.noreply.github.com>
2023-04-22 19:33:35 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -3,8 +3,7 @@ from flask import Flask, request, render_template, url_for
 import pickle
 
 app = Flask(__name__,static_url_path='/static')
-sc = pickle.load(open('sc.pkl', 'rb'))
-model = pickle.load(open('model.pkl', 'rb'))
+rf2 = pickle.load(open('rf2.pkl', 'rb'))
 
 
 @app.route('/')
@@ -19,6 +18,13 @@ def add_header(response):
 @app.route('/predict',methods=['POST'])
 def predict():
     lst = []
+    age = int(request.form["Enter age"])
+    lst += [age]
+    sex = int(request.form["Enter Sex"])
+    if sex == 0:
+        lst += [1 , 0]
+    else:
+        lst += [0 ,1 ]
     cp = int(request.form['chest pain type (4 values)'])
     if cp == 0:
         lst += [1 , 0 ,0 ,0]
@@ -51,8 +57,40 @@ def predict():
         lst += [1 , 0]
     else:
         lst += [0 ,1 ]
+    oldpeak = float(request.form["ST depression induced by exercise relative to rest"])
+    if oldpeak == 0:
+        lst += [1 , 0]
+    else:
+        lst += [0 ,1 ]
+    slope = int(request.form["Slope (values 0,1,2)"])
+    if slope == 0:
+        lst += [1 ,0 ,0]
+    elif slope == 1:
+        lst += [0 ,1 ,0]
+    else:
+        lst += [0 , 0,1]
+    ca = int(request.form['Ca values(0,1,2,3,4)'])
+    if ca == 0:
+        lst += [1 , 0 ,0 ,0,0]
+    elif ca == 1:
+        lst += [0 ,1 ,0 ,0,0]
+    elif ca == 2:
+        lst += [0 ,0 ,1 ,0,0]
+    elif ca == 3:
+        lst += [0 ,0 ,0 ,1,0]
+    elif ca >= 3:
+        lst += [0 ,0 ,0 , 0, 1]
+    thal = int(request.form['Thal values(0,1,2,3)'])
+    if thal == 0:
+        lst += [1 , 0 ,0 ,0]
+    elif thal == 1:
+        lst += [0 ,1 ,0 ,0]
+    elif thal == 2:
+        lst += [0 ,0 ,1 ,0]
+    elif thal >= 3:
+        lst += [0 ,0 ,0 ,1]
     final_features = np.array([lst])
-    pred = model.predict( sc.transform(final_features))
+    pred = rf2.predict(final_features)
     return render_template('result.html', prediction = pred)
 
 if __name__ == "__main__":
